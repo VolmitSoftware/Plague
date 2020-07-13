ede70a7ab3902fbf4a9ddc045f46bcb0c9a9b689
Daniel Mills <danielmillst@gmail.com>
2020-07-13 09:17:34 -0400

    d

---
+++ src/main/java/com/volmit/plague/Plague.java
@@ -0,0 +1,16 @@
+package com.volmit.plague;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plague extends JavaPlugin
+{
+	public void onEnable()
+	{
+		
+	}
+	
+	public void onDisable()
+	{
+		
+	}
+}
